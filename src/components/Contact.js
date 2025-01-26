f52bdbdf10e52ac07a7a15bf62269cb2396a9436
suhani903 <suhanipadmani.it22@scet.ac.in>
2025-01-26 08:51:05 +0530

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 function Contact() {
   return (
     <div className='main'>
-      <h1>you can not try to maked the my contact because you are not konwn me.</h1>
+      <h1>you can not try to maked the my contact because you are not known me.</h1>
       <h2>here you go to contact me : <a href='123456789' className='context'>123456789</a></h2>
     </div>
   )
